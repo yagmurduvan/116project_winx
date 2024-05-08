bea5c1cbbb374bba825d8ef3873ee245634340d0
Merve Topal <mervtpll@gmail.com>
2024-05-08 22:08:13 +0300

    taskMaker

--- Project/src/Reader.java
+++ Project/src/Reader.java
@@ -58,6 +58,60 @@ public class Reader {
             System.out.println("Problem: file not read please write absolute path")	;
         }
     }
+    public static void taskMaker(String file1Name) {//declare the task
+        ArrayList<String> readers = new ArrayList<String>();
+        readFileForTask(file1Name,readers);
+        boolean isFinished=true;
+        for(int i=1;i<readers.size();i++) {
+            while(isFinished&&!isInteger(readers.get(i))) {
+                ArrayList<String> taskInfos = new ArrayList<>();
+                taskInfos.add(readers.get(i));
+                int index=i;
+
+                while(true) {
+
+                    if(index==readers.size()-1) {
+                        System.out.println("son nesne oluşturuldu");
+
+                        if(!isInteger(readers.get(index))) {
+
+                            Task t=null;
+                            t=new Task(readers.get(index));
+                            // System.out.println(t.toString());
+                            tasks.add(t);
+                        }
+                        isFinished=false;
+                        i=readers.size();
+                        break;
+                    }
+                    index++;
+                    if(isInteger(readers.get(index))) {
+                        taskInfos.add(readers.get(index));
+                        System.out.println("nesne oluştur normal");
+                        Task t =null;
+                        t=new Task(readers.get(index-1),Integer.parseInt(readers.get(index)));
+                        //System.out.println(t.toString());
+                        tasks.add(t);
+
+
+                    }else if(index!=readers.size()-1){
+                        i=index;
+                        if(isInteger(readers.get(index+1))) {
+                            break;
+                        }
+                        else {
+                            Task t=null;
+                            t=new Task(readers.get(index));
+                            // System.out.println(t.toString());
+                            tasks.add(t);
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
 
 
 }
