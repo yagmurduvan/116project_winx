87ae8b95bfcb57f7ba88aeddc967322b2582831a
Havvanur777 <121576054+Havvanur777@users.noreply.github.com>
2024-05-08 22:13:52 +0300

    Controller function
    We create a controller function for the checking and assinging the task to jobs that in workflow file

--- Project/src/Reader.java
+++ Project/src/Reader.java
@@ -109,11 +109,87 @@ public class Reader {
                 }
             }
         }
-
-
     }
 
-
+    public static void controller(String [] array2) {
+        int index_job=0;
+        int index_task=0;
+        String element=null;
+        boolean bool=true;
+        int i=0;
+        while(index_job<jobs.size()) {
+            if(array2[i].contains("))")) {
+                break;
+            }
+            if(array2[i].equals(jobs.get(index_job).getJobTypeID())) {
+                i++;
+                while(index_task<tasks.size()) {
+                    if(array2[i].equals(tasks.get(index_task).getType())) {
+                        i++;
+                        if(isInteger(array2[i])) {
+                            if(tasks.get(index_task).getSize()==0) {
+                                tasks.get(index_task).setSize(Integer.parseInt(array2[i]));
+                                jobs.get(index_job).addTask(tasks.get(index_task));
+                                index_task=0;
+                            }else {
+                                jobs.get(index_job).addTask(tasks.get(index_task));
+                                index_task=0;
+                            }
+                        }else if(array2[i].contains(")")) {
+                            element = array2[i].replace(")", "");
+                            if(isInteger(element)) {
+                                if(tasks.get(index_task).getSize()==0) {
+                                    tasks.get(index_task).setSize(Integer.parseInt(element));
+                                    jobs.get(index_job).addTask(tasks.get(index_task));
+                                    index_task=0;
+                                }else {
+                                    jobs.get(index_job).addTask(tasks.get(index_task));
+                                    index_task=0;
+                                }
+                            }
+                            index_job=0;
+                            i++;
+                            break;
+                        }else if(array2[i].contains("))")) {
+                            element = array2[i].replace("))", "");
+                            if(isInteger(element)) {
+                                tasks.get(index_task).setSize(Integer.parseInt(element));
+                                jobs.get(index_job).addTask(tasks.get(index_task));
+                                index_task=0;
+                            }else {
+                                jobs.get(index_job).addTask(tasks.get(index_task));
+                                index_task=0;
+                            }
+                            index_job=0;
+                            break;
+                        }else {
+                            if(tasks.get(index_task).getSize()==0) {
+                                tasks.get(index_task).setSize(1);
+                                jobs.get(index_job).addTask(tasks.get(index_task));
+                                index_task=0;
+                                i--;
+                            }else {
+                                jobs.get(index_job).addTask(tasks.get(index_task));
+                                index_task=0;
+                                i--;
+                            }
+                        }
+                        i++;
+                    }else {
+                        index_task++;
+                    }
+                }
+            }else {
+                index_job++;
+            }
+            if(i==array2.length) {
+                i--;
+            }
+        }
+        for (Job j : jobs) {
+            j.printTasklist();
+        }
+    }
 }
 
 
