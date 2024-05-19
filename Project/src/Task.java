0597793318a5820b11c37a83e6592899e69b53d1
Yağmur Duvan <yagmurduvan21@gmail.com>
2024-05-19 22:34:30 +0300

    task size condition were added

--- Project/src/Task.java
+++ Project/src/Task.java
@@ -1,17 +1,43 @@
 package Project.src;
 
-import java.util.ArrayList;
-
 public class Task {
     private String type;
     private double size;
     private int speed;
     private double minplus;
+    private double duration ;
+    private double finishTime;
+    private boolean state;
+    ;
+    public boolean isState() {
+        return state;
+    }
+    public void setState(boolean state) {
+        this.state = state;
+    }
     public Task(String type, double size) {
+        if (size < 0) {
+            throw new IllegalArgumentException("Size cannot be negative.");
+        }
         this.type = type;
         this.size = size;
         this.speed=getSpeed();
         this.minplus=getMinplus();
+        this.duration=getDuration();
+        this.finishTime=getFinishTime();
+        this.state=true;
+    }
+    public double getFinishTime() {
+        return finishTime;
+    }
+    public void setFinishTime(double finishTime) {
+        this.finishTime =finishTime+ getFinishTime();
+    }
+    public double getDuration() {
+        return duration;
+    }
+    public void setDuration(double duration) {
+        this.duration = duration;
     }
     public double getMinplus() {
         return minplus;
@@ -44,5 +70,8 @@ public class Task {
     @Override
     public String toString() {
         return "Task [" + (type != null ? "type=" + type + ", " : "") + "size=" + size + "]";
+    }
+    public void spendTime(double time) {
+        setFinishTime(getFinishTime()+time);
     }
 }
